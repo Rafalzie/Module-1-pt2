ca0a4fa16c263727e02ee8f6fd394431ea6062ad
Rafalzie <rafal19191996@gmail.com>
2024-08-26 01:12:55 +0000

    Add files via upload

---
+++ module1-pt2.js
@@ -0,0 +1,18 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Welocme to the Party!";
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+  alert("get your ticket!");
+} else {
+  alert("What would you like to get…");
+}
+let myVariable = document.querySelector("h1");
+alert("welcome to the party!");
+function multiply(num1, num2) {
+    alert("hello!");
+    let result = num1 * num2;
+    return result;
+  }
+  document.querySelector("html").addEventListener("click", function () {
+    alert("Welocme to the Walmart!");
+  });
